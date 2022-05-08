2150d69a5f459415305dd5d169b0d9c3738e72c4
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-05-08 16:48:51 +0900

    HTML, CSS, JS toTopBtn

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ const menuList = document.querySelector(
 menuBtn.addEventListener('click', () => {
   menuList.classList.toggle('active');
 });
+
+const frontImgsHeight = document
+  .querySelector('#frontImgs')
+  .getBoundingClientRect().height;
+console.log(frontImgsHeight);
+
+const toTopBtn = document.querySelector('.toTopBtn');
+
+toTopBtn.addEventListener('click', () => {
+  const home = document.querySelector('#navBarAndTopInfo');
+  if (window.scrollY > frontImgsHeight)
+    home.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (window.scrollY > frontImgsHeight) toTopBtn.classList.add('visible');
+  else toTopBtn.classList.remove('visible');
+});
