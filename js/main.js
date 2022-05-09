c1a591105d344813b2f1d5cf99e3f8059d97ede1
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-05-10 00:32:19 +0900

    JS add quotes

--- js/main.js
+++ js/main.js
@@ -28,15 +28,3 @@ document.addEventListener('scroll', () => {
   if (window.scrollY > frontImgsHeight) toTopBtn.classList.add('visible');
   else toTopBtn.classList.remove('visible');
 });
-
-const clock = document.querySelector('.tel');
-function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-getClock();
-setInterval(getClock, 1000);
