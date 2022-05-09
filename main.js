25b98e326d280809566c0887749e55ea8d015a15
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-05-09 23:33:57 +0900

    JS add clock

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ menuBtn.addEventListener('click', () => {
 const frontImgsHeight = document
   .querySelector('#frontImgs')
   .getBoundingClientRect().height;
-console.log(frontImgsHeight);
+// console.log(frontImgsHeight);
 
 const toTopBtn = document.querySelector('.toTopBtn');
 
@@ -22,8 +22,21 @@ toTopBtn.addEventListener('click', () => {
     home.scrollIntoView({ behavior: 'smooth' });
 });
 
+console.log(`frontImgsHeight! : ${frontImgsHeight}`);
 document.addEventListener('scroll', () => {
   console.log(window.scrollY);
   if (window.scrollY > frontImgsHeight) toTopBtn.classList.add('visible');
   else toTopBtn.classList.remove('visible');
 });
+
+const clock = document.querySelector('.tel');
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
